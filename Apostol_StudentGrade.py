4eed3dae7faa9f73d61ca1889990dfe4129071f4
Ducky-Quack <apostoljayphie@gmail.com>
2024-10-18 16:26:49 +0800

    Update Apostol_StudentGrade.py

--- Apostol_StudentGrade.py
+++ Apostol_StudentGrade.py
@@ -3,17 +3,17 @@ section = input("What is your section? (ex. IT-101)\n")
 
 prelim = float(input("What is your preliminary grade: "))
 midterm = float(input("What is your midterm grade: "))
-final = float(input("What is your finals grade: "))
+final = float(input("What is your final grade: "))
 
 # Checking for each grade condition
 while not (40 <= prelim <= 100) or not (40 <= midterm <= 100) or not (40 <= final <= 100):
-    print("You have inputted invalid number.")
+    print("\nYou have inputted an invalid number(s).")
     if not (40 <= prelim <= 100):
         prelim = float(input("Please enter your PRELIMINARY grade again.\nPreliminary grade: "))
     if not (40 <= midterm <= 100):
         midterm = float(input("Please enter your MIDTERM grade again.\nMidterm grade: "))
     if not (40 <= final <= 100):
-        final = float(input("Please enter your FINALS grade again.\nFinals grade: "))
+        final = float(input("Please enter your FINAL grade again.\nFinal grade: "))
 
 # Final grade computation
 final_grade = round((prelim * 0.33) + (midterm * 0.33) + (final * 0.33) + 1)
@@ -41,6 +41,6 @@ else:
     GPA, description = 5.00, "Failed"
 
 # Print final grade, GPA, and description
-print(f"Hello, {name} (from {section})!")
+print(f"\nHello, {name} (from {section})!")
 print(f"Your final grade is: {final_grade}%")
-print(f"Your GPA is {GPA} ({description})")
+print(f"Your GPA is {GPA} ({description})\n")
